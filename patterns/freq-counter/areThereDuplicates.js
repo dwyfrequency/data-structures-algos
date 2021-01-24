d3f518bedf84b6c3c3c8fde6b1795e950670b8c3
dwyfrequency <jdwy215@gmail.com>
2021-01-24 14:48:43 -0500

    add new solution

---
+++ patterns/freq-counter/areThereDuplicates.js
@@ -0,0 +1,14 @@
+// Space O(n) | Time O(n) - time to create set from args
+function areThereDuplicatesWithSet(...args) {
+  return args.length !== new Set(args).size;
+}
+
+// Space O(n) | Time O(n) -
+function areThereDuplicatesWithMap(...args) {
+  const freqCnt = {};
+  for (const arg of args) {
+    if (freqCnt[arg]) return true;
+    freqCnt[arg] = true;
+  }
+  return false;
+}
