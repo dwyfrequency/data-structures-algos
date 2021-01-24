91cc5188e92af5d8cab1cae761c128663fea65b5
dwyfrequency <jdwy215@gmail.com>
2021-01-24 12:34:40 -0500

    additional solution with for loops

--- patterns/sliding-window/maxSubArray.js
+++ patterns/sliding-window/maxSubArray.js
@@ -10,9 +10,10 @@
  * @param {int[]} arr
  * @param {int} subArrSize
  */
+// Space O(1) | Time O(N)
 // (int[], int) -> int|null
-function maxSubArray(arr, subArrSize) {
-  if (!arr.length) return null;
+function maxSubArrayInitial(arr, subArrSize) {
+  if (!arr.length || arr.length < subArrSize) return null;
   let maxSum = 0,
     currWindowSum = 0;
   let left = 0,
@@ -33,6 +34,27 @@ function maxSubArray(arr, subArrSize) {
   return maxSum;
 }
 
+function maxSubArray(arr, subArrSize) {
+  if (!arr.length || arr.length < subArrSize) return null;
+  let maxSum;
+  let currWindowSum = 0;
+  // get initial window sum
+  for (let i = 0; i < subArrSize; i++) {
+    currWindowSum += arr[i];
+  }
+  // assign initial max sum from first window
+  maxSum = currWindowSum;
+
+  // initialize rightmost value to size of window
+  // decrement left most window value and add rightmost value
+  for (let i = subArrSize; i < arr.length; i++) {
+    currWindowSum = currWindowSum - arr[i - subArrSize] + arr[i];
+    maxSum = Math.max(maxSum, currWindowSum);
+  }
+
+  return maxSum;
+}
+
 console.log(maxSubArray([1, 3, 7], 2)); // 10
 console.log(maxSubArray([10, 3, 7, 4], 2)); // 13
-console.log(maxSubArray([10, 4], 3)); // 14
+console.log(maxSubArray([10, 4, 2], 2)); // 14
