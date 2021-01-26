b338ffe2342ecf935a7ef01e63b7209ac5415a7e
dwyfrequency <jdwy215@gmail.com>
2021-01-25 19:07:47 -0500

    adding func signature

---
+++ patterns/sliding-window/minSubArray.js
@@ -0,0 +1,7 @@
+// Space O(1) | Time O(N)
+// (int[], int) -> int|null
+function minSubArray(arr, subArrSize) {}
+
+console.log(minSubArray([1, 3, 7], 2)); // 4
+console.log(minSubArray([10, 3, 7, 4], 2)); // 10
+console.log(minSubArray([10, 4, 2], 2)); // 6
