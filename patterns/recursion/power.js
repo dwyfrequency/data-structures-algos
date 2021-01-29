82953651a497c4fc0581cf2f295a1ae54b69a8ee
dwyfrequency <jdwy215@gmail.com>
2021-01-28 20:29:23 -0500

    removing commented sol

--- patterns/recursion/power.js
+++ patterns/recursion/power.js
@@ -10,11 +10,6 @@ function power(base, exponent) {
   return base * power(base, exponent - 1);
 }
 
-// function power(base, exponent) {
-//   if (exponent === 0) return 1;
-//   return base * power(base, exponent - 1);
-// }
-
 console.log(power(3, 3)); // 27
 console.log(power(2, 4)); // 16
 console.log(power(4, 1)); // 4
