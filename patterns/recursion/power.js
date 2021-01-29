f7c5591e08d54dcdcfa05a287ee3e7c2fda06f27
dwyfrequency <jdwy215@gmail.com>
2021-01-28 20:28:43 -0500

    adding solution

---
+++ patterns/recursion/power.js
@@ -0,0 +1,20 @@
+/**
+ * Mimic Math.pow with recursion
+ * @param {int} base
+ * @param {int} exponent
+ */
+function power(base, exponent) {
+  if (exponent === 0) {
+    return 1;
+  }
+  return base * power(base, exponent - 1);
+}
+
+// function power(base, exponent) {
+//   if (exponent === 0) return 1;
+//   return base * power(base, exponent - 1);
+// }
+
+console.log(power(3, 3)); // 27
+console.log(power(2, 4)); // 16
+console.log(power(4, 1)); // 4
