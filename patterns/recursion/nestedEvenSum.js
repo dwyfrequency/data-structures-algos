55f30bf72def33fa04e523db9cd14fe64f290e0b
dwyfrequency <jdwy215@gmail.com>
2021-01-30 12:57:34 -0500

    add solution and notes

--- patterns/recursion/nestedEvenSum.js
+++ patterns/recursion/nestedEvenSum.js
@@ -1,5 +1,22 @@
-function nestedEvenSum() {
-  // add whatever parameters you deem necessary - good luck!
+/**
+ * Return the sum of all even numbers in an object which may contain nested
+ * objects.
+ * @param {*} obj1
+ */
+// (Object, Object) -> int
+function nestedEvenSum(obj) {
+  return Object.values(obj).reduce((accum, curr) => {
+    if (typeof curr === 'number' && curr % 2 === 0) {
+      // sum any even numbers
+      return accum + curr;
+    } else if (typeof curr === 'object') {
+      // unpack sum any inner objects
+      return accum + nestedEvenSum(curr);
+    } else {
+      // skip any non-nums or objects
+      return accum;
+    }
+  }, 0);
 }
 
 var obj1 = {
@@ -22,5 +39,5 @@ var obj2 = {
   e: { e: { e: 2 }, ee: 'car' },
 };
 
-nestedEvenSum(obj1); // 6
-nestedEvenSum(obj2); // 10
+console.log(nestedEvenSum(obj1)); // 6
+console.log(nestedEvenSum(obj2)); // 10
