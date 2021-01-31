978eb5eeb15b55648f42dcae095619442c5793b7
dwyfrequency <jdwy215@gmail.com>
2021-01-31 18:05:06 -0500

    add binary search

---
+++ topics/search/binary/binarySearch.js
@@ -0,0 +1,28 @@
+// (arr) -> boolean
+function binarySearch(arr, searchVal) {
+  let left = 0;
+  let right = arr.length - 1;
+  while (left <= right) {
+    let pivotIndex = Math.floor((right - left) / 2) + left;
+    const pivotValue = arr[pivotIndex];
+    if (searchVal === pivotValue) {
+      return pivotIndex;
+    } else if (searchVal > pivotValue) {
+      // reduce new window size excluding the pivot value as pivot val has
+      // already been determined to be not the answer
+      left = pivotIndex + 1;
+    } else {
+      // reduce new window size excluding the pivot value as pivot val has
+      // already been determined to be not the answer
+      right = pivotIndex - 1;
+    }
+  }
+  return -1;
+}
+
+console.log(binarySearch([1, 2, 8, 9], 9)); // true
+console.log(binarySearch([1, 2, 4, 6, 7, 8, 9], 9)); // true
+console.log(binarySearch([1, 2, 4, 6, 7, 8, 9], 2)); // true
+console.log(binarySearch([1, 2, 4, 7, 8, 9], 2)); // true
+console.log(binarySearch([1, 2, 4, 6, 7, 8, 9], 19)); // false
+console.log(binarySearch([1, 2, 4, 6, 7, 9], 3)); // false
