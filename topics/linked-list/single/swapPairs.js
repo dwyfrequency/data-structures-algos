edf5daf19251598dd85216b46f5f06ccfd2cebfd
dwyfrequency <jdwy215@gmail.com>
2021-02-05 21:16:50 -0500

    saqué la basura

--- topics/linked-list/single/swapPairs.js
+++ topics/linked-list/single/swapPairs.js
@@ -5,9 +5,9 @@ function swapPairs(head) {
   // this would occur in lists with odd number of values
   if (head === null || head.next === null) return head;
 
-  const prevHead = head; // 3
-  const tempNextChain = prevHead.next.next; // null
-  const newHead = prevHead.next; // 4
+  const prevHead = head;
+  const tempNextChain = prevHead.next.next;
+  const newHead = prevHead.next;
   newHead.next = prevHead;
   // add the next attribute as the new head of the sub-list
   prevHead.next = swapPairs(tempNextChain);
@@ -19,3 +19,4 @@ console.log(
     new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, null))))
   )
 );
+// [1, 2, 3, 4] -> [2,1,4,3]
