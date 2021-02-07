6add1caede3ceab8e9de67c548891add86ba04c4
dwyfrequency <jdwy215@gmail.com>
2021-02-07 10:45:57 -0500

    add solution

--- topics/trees/BST/searchBST.js
+++ topics/trees/BST/searchBST.js
@@ -6,5 +6,15 @@
  * @returns
  */
 function searchBST(root, val) {
+  // if we receive null root then the value was not found
+  if (root === null) return null;
+  // if we found the value, return the root
   if (root.val === val) return root;
+  // if we find the value, we'll get the root it's in
+  const left = searchBST(root.left, val);
+  if (left) return left;
+  const right = searchBST(root.right, val);
+  if (right) return right;
+  // if the value doesn't exist in the branches, return null
+  return null;
 }
