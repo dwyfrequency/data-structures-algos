fe8b53c5b188ccd70b63691fa2a5253dce8d4f3f
dwyfrequency <jdwy215@gmail.com>
2021-02-07 10:37:47 -0500

    add common modules

---
+++ topics/trees/BST/searchBST.js
@@ -0,0 +1,10 @@
+/**
+ * Find the node in the BST that the node's value equals val and return the
+ * subtree rooted with that node. If such a node does not exist, return null.
+ * @param {*} root
+ * @param {number} val
+ * @returns
+ */
+function searchBST(root, val) {
+  if (root.val === val) return root;
+}
---
+++ topics/trees/common/TreeNode.js
@@ -0,0 +1,10 @@
+module.exports = class TreeNode {
+  val;
+  left;
+  right;
+  constructor(val, left, right) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+};
