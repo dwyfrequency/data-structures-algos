44cdfa6117f6280abf3ebc188c8babb4596c1480
dwyfrequency <jdwy215@gmail.com>
2021-02-07 11:43:13 -0500

    add initial idea for approach

---
+++ topics/recursion/pascalsTriangle2.js
@@ -0,0 +1,28 @@
+/*
+Approach:
+If index is 0, value is 1.
+else it's the sum of the adjacent pairs
+
+Pointer at initial value
+  add to array
+  after, peek if there is a value
+    if so, sum and add to array
+    move pointer
+Examples:
+[1]
+[1, 1]
+[1, 2, 1]
+*/
+
+/**
+ * @param {number} rowIndex
+ * @return {number[]}
+ */
+function getRowRecurse(rowIndex) {
+  const row = [];
+
+  function collectRowValues(currIndex) {}
+  return;
+}
+
+function getRowIterative(rowIndex) {}
