be7451d871d6772901a7d4f556e7565331c093e6
dwyfrequency <jdwy215@gmail.com>
2021-02-17 20:12:33 -0500

    add queue implementation

---
+++ topics/queue/shared/queue.js
@@ -0,0 +1,46 @@
+class Node {
+  constructor(val, next = null) {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+class Queue {
+  constructor(first = null, last = null, length = 0) {
+    this.first = first;
+    this.last = last;
+    this.length = length;
+  }
+
+  enqueue(val) {
+    const node = new Node(val);
+    if (!this.first) {
+      this.first = node;
+      this.last = node;
+    } else {
+      const nextNode = this.last;
+      nextNode.next = node;
+      this.last = node;
+    }
+
+    this.length++;
+    return this;
+  }
+
+  dequeue() {
+    if (!this.first) throw new Error('Empty list!!!');
+    const removeNode = this.first;
+    this.first = this.first.next;
+
+    removeNode.next = null;
+    this.length--;
+    return removeNode;
+  }
+}
+
+const queue = new Queue();
+queue.enqueue(1);
+queue.enqueue(3);
+// console.log(queue);
+console.log(queue.dequeue());
+console.log(queue);
